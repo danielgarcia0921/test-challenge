b645ab124a6851514d1b2712475ecb926eadddca
danielgarcia0921 <daniel.garcia2@dexcom.com>
2022-12-22 08:20:22 -0800

    Update Strings

--- src/test/java/utils/AssertStrings.java
+++ src/test/java/utils/AssertStrings.java
@@ -7,13 +7,12 @@ public final class AssertStrings {
     public static final String NEWPOSTTITLE = "Test";
     public static final String NEWPOSTSUBTITLE = "Test Subtitle";
     public static final String NEWPOSTDESCRIPTION = "This is a test description :)";
-    public static final String POSTSHEADER = "POSTS";
+    public static final String EDITPOSTTITLE = "Edited Post";
+    public static final String EDITPOSTSUBTITLE = "This is an edited post";
+    public static final String EDITPOSTDESCRIPTION = "Teehee haha";
 
     //URLs
     public static final String POSTSPAGEURL = "https://sheltered-mesa-19374.herokuapp.com/posts/";
-//    public static final String NEWPOSTTITLE = "Test";
-//    public static final String NEWPOSTTITLE = "Test";
-//    public static final String NEWPOSTTITLE = "Test";
 
     //Date Time
     public static final String dateTimeFormat = "MMM. dd, yyyy, h:mm a";
